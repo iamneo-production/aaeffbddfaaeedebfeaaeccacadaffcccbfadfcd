83544e1a28e9aea907712b4b49f35a625eadcf50
Iamneo <techadmin@iamneo.ai>
2023-08-11 12:20:40 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
-  fromCurr :String = '';
+  fromCurr :string = '';
   toCurr   :string = '';
   amount !:number;
   result: string = 'sample';
